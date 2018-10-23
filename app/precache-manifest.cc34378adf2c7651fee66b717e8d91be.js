29771fab64d8aa940d252d57b5ef1589654a0bfa
Jonathan Ayala <yojona@msn.com>
2018-10-23 12:51:04 -0700

    Add index.html for test

---
+++ app/precache-manifest.cc34378adf2c7651fee66b717e8d91be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e8c95dcafad9a893d0fa2b30c4752218",
+    "url": "/static/media/pad.e8c95dca.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2dd6684f5748dc9bfe78",
+    "url": "/static/js/main.2dd6684f.chunk.js"
+  },
+  {
+    "revision": "7b74f30e650ec5fe3936",
+    "url": "/static/js/1.7b74f30e.chunk.js"
+  },
+  {
+    "revision": "3117bd6f8c593002dded8406aad3e9b3",
+    "url": "/index.html"
+  }
+];
